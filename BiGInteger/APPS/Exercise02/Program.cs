7d70cb071dc4b361a7e1bb499f9d2738a068fbb1
ammwangi <elijah.mwangi@kamsware.com>
2022-09-12 07:34:52 +0300

    simple console app that reads  input from user and returns in words

--- BiGInteger/APPS/Exercise02/Program.cs
+++ BiGInteger/APPS/Exercise02/Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Exercise01;
+using System.Numerics;
+
+namespace Excercise01
+{
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConsoleKeyInfo cki;
+            do
+            {
+                Console.WriteLine("Input a number to be displayed in words:");
+                var input = Console.ReadLine();
+                if (!string.IsNullOrWhiteSpace(input))
+                {
+                    if (BigInteger.TryParse(input, out BigInteger bigInteger))
+                    {
+                        var toWords = bigInteger.ToWords();
+                        Console.WriteLine($"Your number in words is: {toWords}");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Please enter a valid input");
+                    }
+
+                }
+                else
+                {
+                    Console.WriteLine("Please enter a valid input");
+                }
+                Console.WriteLine("Press ESC to end or any key to try again.");
+                cki = Console.ReadKey();
+                Console.WriteLine();
+            } while (cki.Key != ConsoleKey.Escape);
+
+        }
+    }
+}
