67583b7dda12c0103891587d65061b74947a9f0d
ammwangi <elijah.mwangi@kamsware.com>
2022-09-12 07:15:26 +0300

    Convert to words , in  word format

--- BiGInteger/LIB/Exercise01/CustomeIntergerExtensions.cs
+++ BiGInteger/LIB/Exercise01/CustomeIntergerExtensions.cs
@@ -1,10 +1,41 @@
-﻿namespace Exercise01
+﻿using System.Numerics;
+
+namespace Exercise01
 {
     public class CustomeIntergerExtensions
     {
         //The Referece of this solution is from // https://docs.microsoft.com/en-us/dotnet/api/system.numerics.biginteger?view=net-6.0
+        
 
+        //This separates via modulas to determine the  'AND" In the Words
+        public static string GenerateEndPart(BigInteger value, BigInteger divider)
+        {
+            BigInteger modulus = value % divider;
+            string endPart = modulus == 0 ? "" : $"{(divider == 100 ? "and " : "")}{ConvertToWords(modulus)}";
+            return endPart;
+        }
 
+        public static string ConvertToWords(BigInteger value)
+        {
+            string[] uniqueDigits = new string[] {
+                "zero", "one", "two", "three", "four",
+                "five", "six", "seven", "eight", "nine",
+                "ten", "eleven", "twelve",
+                "thirteen",  "fourteen", "fifteen", "sixteen",
+                "seventeen", "eighteen", "nineteen"
+            };
+
+            string[] tensMultiple = new string[] {
+                "", "", "twenty",  "thirty", "forty",
+                "fifty", "sixty", "seventy", "eighty", "ninety"
+            };
+
+            string[] tensPower = new string[] {
+                "hundred", "thousand", "million", "billion", "trillion", "quadrillion", "quintillion"
+            };
+            return null;
+        
+        }
 
     }
 }
--- EmployeeHierachy/Tests/EmployeeTests/TestEmployees.cs
+++ EmployeeHierachy/Tests/EmployeeTests/TestEmployees.cs
@@ -1,3 +1,5 @@
+using EmployeeHierachy;
+
 namespace EmployeeTests
 {
     [TestClass]
