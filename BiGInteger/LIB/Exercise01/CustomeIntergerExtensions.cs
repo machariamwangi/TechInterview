ede667990e2ef49763994477770d225ab85c5371
ammwangi <elijah.mwangi@kamsware.com>
2022-09-12 07:11:48 +0300

    BigInteger Tests

---
+++ BiGInteger/LIB/Exercise01/CustomeIntergerExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace Exercise01
+{
+    public class CustomeIntergerExtensions
+    {
+        //The Referece of this solution is from // https://docs.microsoft.com/en-us/dotnet/api/system.numerics.biginteger?view=net-6.0
+
+
+
+    }
+}
