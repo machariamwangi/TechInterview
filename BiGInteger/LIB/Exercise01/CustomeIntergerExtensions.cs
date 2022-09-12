11a743d5ebca38f4b38b3875967987150f32323d
ammwangi <elijah.mwangi@kamsware.com>
2022-09-12 07:19:55 +0300

    Quintillion and values larger than what a Int64 CAN HOLD

--- BiGInteger/LIB/Exercise01/CustomeIntergerExtensions.cs
+++ BiGInteger/LIB/Exercise01/CustomeIntergerExtensions.cs
@@ -71,7 +71,50 @@ namespace Exercise01
                 return $"{ConvertToWords(value / divider)} {tensPower[2]} {GenerateEndPart(value, divider)}";
             }
 
-            return null;
+            // Billion
+            if (value < 1_000_000_000_000)
+            {
+                int divider = 1_000_000_000;
+                return $"{ConvertToWords(value / divider)} {tensPower[3]} {GenerateEndPart(value, divider)}";
+            }
+
+            // Trillion
+            if (value < 1_000_000_000_000_000)
+            {
+                BigInteger divider = 1_000_000_000_000;
+                return $"{ConvertToWords(value / divider)} {tensPower[4]} {GenerateEndPart(value, divider)}";
+            }
+
+            // Quadrillion
+            if (value < 1_000_000_000_000_000_000)
+            {
+                BigInteger divider = 1_000_000_000_000_000;
+                return $"{ConvertToWords(value / divider)} {tensPower[5]} {GenerateEndPart(value, divider)}";
+            }
+
+            // Quintillion 18,456,002,032,011,000,007 //18456002032011000007
+            if (value < 10_000_000_000_000_000_000)
+            {
+                BigInteger divider = 1_000_000_000_000_000_000;
+                return $"{ConvertToWords(value / divider)} {tensPower[6]} {GenerateEndPart(value, divider)}";
+            }
+            if (value < 18_446_744_073_709_551_615)//18,446,744,073,709,551,615  being the  maxima ulong or big Interger
+            {
+                BigInteger divider = 1_000_000_000_000_000_000;
+                return $"{ConvertToWords(value / divider)} {tensPower[6]} {GenerateEndPart(value, divider)}";
+            }
+            //the biggest integer literal you can define in c# is ulong with max value of 18,446,744,073,709,551,615 (larger values leads to compile error), 
+            //which is obviously not enough in your case, easy solution would be to use BigInteger.Parse 
+            var num = "91389681247993671255432112000000";
+            var longInteger = BigInteger.Parse(num);
+
+            if (value <= longInteger) //18,446,744,073,709,551,615 //
+            {
+                BigInteger divider = 1_000_000_000_000_000_000;
+                return $"{ConvertToWords(value / divider)} {tensPower[6]} {GenerateEndPart(value, divider)}";
+            }
+            //Return any Number bigger than 91389681247993671255432112000000 is not supported
+            return $"{value} is not supported";
 
         }
 
